bf6defa9a05133055423c39510e21d5aa22380ab
luyao1991 <gaopan157@163.com>
2018-08-05 23:35:43 +0800

    更新Django项目

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # learning_log
 使用python-Django编写web应用程序
+8.5
+learning项目-可以在浏览器打开http://127.0.0.1:8000/
+重要的文件setting.py，urls.py，wsgi.py
+setting.py指定Django如何与你的系统交互以及如何管理项目。
+文件urls.py告诉Django应创建哪些网页来响应浏览器请求。
+wsgi.py帮助Django提供它创建的文件，文件名web server gateway interface(Web服务器网关接口)
+8.6
+learning_app属于应用程序项目。
+重要的文件models.py,admin.py,views.py
+models.py来定义在应用程序中管理的数据。
+
+
+
--- learning_app/models.py
+++ learning_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Topic(models.Model):
+    """
+    用户学习的主题
+    text是一个CharField，由字符和文本组成的数据，设置200个字符，就是告诉Django要在数据库预留多少空间。
+    date_added记录时间和日期的数据，传递实参auto_now_add=True，让Django把这个属性自动设置成当前日期和时间。
+    """
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text
+
+
+
+
+
+
